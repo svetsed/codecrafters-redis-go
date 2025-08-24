869efaa728abd5b6af689542da8457204a3cb21d
melvaalm <melvaalm@student.21-school.ru>
2025-08-24 04:00:33 +0300

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -4,16 +4,23 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"strconv"
 	"strings"
 	"sync"
+	"time"
 )
 
 // Ensures gofmt doesn't remove the "net" and "os" imports in stage 1 (feel free to remove this!)
 var _ = net.Listen
 var _ = os.Exit
 
+type entry struct {
+	value 	  string
+	expiresAt int64
+}
+
 var (
-	store = make(map[string]string)
+	store = make(map[string]entry)
 	mute sync.RWMutex
 )
 func main() {
@@ -39,7 +46,7 @@ func main() {
 func handleConn(conn net.Conn) {
 	defer conn.Close()
 
-	buf := make([]byte, 2048)
+	buf := make([]byte, 1024)
 	for {
 		_, err := conn.Read(buf)
 		if err != nil {
@@ -62,12 +69,25 @@ func handleConn(conn net.Conn) {
 			if len(sliceCom) < 7 {
 				if _, err := conn.Write([]byte("-ERR wrong number of arguments for 'set' command\r\n")); err != nil {
 					fmt.Println("Error writing response 'ERR' on set: ", err.Error())
-					return
+				}
+				return
+			} else if len(sliceCom) == 8 {
+				mute.Lock()
+				store[sliceCom[4]] = entry{value: sliceCom[6], expiresAt: -1}
+				mute.Unlock()
+			} else if len(sliceCom) == 12 {
+				if strings.ToLower(sliceCom[8]) == "px" {
+					var expiresAt int64 = -1
+					ms, _ := strconv.Atoi(sliceCom[10])
+					if ms > 0 {
+						expiresAt = time.Now().UnixNano()/1e6 + int64(ms)
+					}
+					mute.Lock()
+					store[sliceCom[4]] = entry{value: sliceCom[6], expiresAt:expiresAt}
+					mute.Unlock()
 				}
 			}
-			mute.Lock()
-			store[sliceCom[4]] = sliceCom[6]
-			mute.Unlock()
+			
 			if _, err := conn.Write([]byte("+OK\r\n")); err != nil {
 				fmt.Println("Error writing response 'OK' on set: ", err.Error())
 				return
@@ -76,19 +96,33 @@ func handleConn(conn net.Conn) {
 			if len(sliceCom) < 5 {
 				if _, err := conn.Write([]byte("-ERR wrong number of arguments for 'get' command\r\n")); err != nil {
 					fmt.Println("Error writing response 'ERR' on get: ", err.Error())
-					return
 				}
+				return
 			}
+
 			mute.RLock()
 			val, ok := store[sliceCom[4]]
 			mute.RUnlock()
+
 			if !ok {
-				if _, err := conn.Write([]byte("-1\r\n")); err != nil {
+				if _, err := conn.Write([]byte("$-1\r\n")); err != nil {
 					fmt.Println("Error writing response 'no key' on get: ", err.Error())
-					return
 				}
+				return
 			}
-				fmt.Fprintf(conn, "$%d\r\n%s\r\n", len(val), val)
+			
+			if val.expiresAt != -1 && time.Now().UnixNano()/1e6 > val.expiresAt {
+				mute.Lock()
+				delete(store, sliceCom[4])
+				mute.Unlock()
+				if _, err := conn.Write([]byte("$-1\r\n")); err != nil {
+					fmt.Println("Error writing response 'no key' on get: ", err.Error())
+				}
+				return
+			} 
+				
+			fmt.Fprintf(conn, "$%d\r\n%s\r\n", len(val.value), val.value)
+
 		}
 	}
 }
